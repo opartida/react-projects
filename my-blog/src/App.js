ce552f73e2388d28ab10e88058b800306e3eef37
Oliver Partida <oliverpartidagutierrez@gmail.com>
2022-08-01 23:38:10 +0200

    feat: add reusable articles list

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,22 +1,28 @@
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
-import ArticlesList from "./pages/ArticlesList";
+import ArticlesListPage from "./pages/ArticlesListPage";
 import ArticlePage from "./pages/ArticlePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./NavBar";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<HomePage/>} exact />
-          <Route path="/about" element={<AboutPage/>} />
-          <Route path="/articles-list" element={<ArticlesList/>} />
-          <Route path="/article" element={<ArticlePage/>} />
-        </Routes>
-      </div>
-    </Router>
+    <>
+      <Router>
+        <div className="App">
+          <NavBar />
+          <div id="page-body">
+            <Routes>
+              <Route path="/" element={<HomePage />} exact />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/articles-list" element={<ArticlesListPage />} />
+              <Route path="/article/:name" element={<ArticlePage />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
+    </>
   );
 }
 
--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import articleContent from "./article-content";
+import ArticlesList from "../components/ArticlesList";
 
-const ArticlePage = ({ match }) => {
-  const name = match.params.name;
+const ArticlePage = () => {
+  const { name } = useParams();
+  const article = articleContent.find((article) => article.name === name);
+  if (!article) return <h1>Article does not exist!</h1>;
+  const otherArticles = articleContent.filter(
+    (article) => article.name !== name
+  );
   return (
     <>
-      <h1>This is {name} article </h1>
+      <h1>{article.title}</h1>
+      {article.content.map((paragraph, key) => (
+        <p key={key}>{paragraph}</p>
+      ))}
+      <h3>Other Articles:</h3>
+      <ArticlesList articles={otherArticles} />
     </>
   );
 };
