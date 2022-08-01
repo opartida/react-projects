ca05f39f502a666a2558be8321c7205cce3880e6
Oliver Partida <oliverpartidagutierrez@gmail.com>
2022-08-02 00:11:01 +0200

    add vcr app

---
+++ office/src/Primitives.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Box, View, Cylinder, Sphere, StyleSheet } from "react-vr";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View>
+        <Box dimWidth={2} 
+        dimDepth={2} 
+        dimHeight={1} 
+        style={{ color: "red" }} />
+      </View>
+    );
+  }
+}
