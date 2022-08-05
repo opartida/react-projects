9bb87368ff5095a673ae859a7bd9c6214246bdc5
Oliver Partida <oliver.partida@sage.com>
2022-08-05 17:42:00 +0200

    fix: add fetch & proxy and comments component

--- my-blog/frontend/src/pages/ArticlePage.js
+++ my-blog/frontend/src/pages/ArticlePage.js
@@ -3,20 +3,37 @@ import { useParams } from "react-router-dom";
 import articleContent from "./article-content";
 import ArticlesList from "../components/ArticlesList";
 import NotFoundPage from "./NotFoundPage";
+import { useState, useEffect } from "react";
+import CommentsList from "../components/CommentsList";
 
 const ArticlePage = () => {
   const { name } = useParams();
   const article = articleContent.find((article) => article.name === name);
-  if (!article) return <NotFoundPage/>;
+
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(`/api/articles/${name}`);
+      const body = await result.json();
+      setArticleInfo(body);
+    };
+    fetchData();
+  }, [name]);
+
+  if (!article) return <NotFoundPage />;
+
   const otherArticles = articleContent.filter(
     (article) => article.name !== name
   );
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This post has been upvoted {articleInfo.upvotes} times</p>
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
+      <CommentsList comments={articleInfo.comments} />
       <h3>Other Articles:</h3>
       <ArticlesList articles={otherArticles} />
     </>
