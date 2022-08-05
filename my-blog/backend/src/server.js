f44e8eb859e98ddc603d11ef6a85ca9bcce94114
Oliver Partida <oliver.partida@sage.com>
2022-08-05 13:12:02 +0200

    fix: add mongodb

--- my-blog/backend/src/server.js
+++ my-blog/backend/src/server.js
@@ -1,17 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-const articlesInfo = {
-  "learn-react": {
-    upvotes: 0,
-  },
-  "learn-node": {
-    upvotes: 0,
-  },
-  "my-thoughts-on-resumes": {
-    upvotes: 0,
-  },
-};
 
 const app = express();
 
@@ -19,10 +8,44 @@ app.use(bodyParser.json());
 
 app.post("/api/articles/:name/upvote", (req, response) => {
   const articleName = req.params.name;
-  articlesInfo[articleName].upvotes += 1;
+
   response
     .status(200)
-    .send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!!`);
+    .send(
+      `${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!!`
+    );
+});
+
+app.post("/api/articles/:name/add-comment", (req, response) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
+  articlesInfo[articleName].comments.push({ username, text });
+  response.status(200).send(articlesInfo[articleName]);
+});
+
+app.get("/api/articles/:name", async (req, response) => {
+  try {
+    const { MongoClient } = require("mongodb");
+    const articleName = req.params.name;
+    const uri = "mongodb://localhost:27017";
+    const client = new MongoClient(uri);
+    
+    await client.connect();
+    
+    const db = client.db("my-blog");
+
+    const articleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+
+    response.status(200).json(articleInfo);
+    
+
+  } catch (error) {
+    response.status(500).json({ message: "Error connecting to db", error });
+  } finally {
+    await client.close();
+  }
 });
 
 app.listen(8000, () => {
