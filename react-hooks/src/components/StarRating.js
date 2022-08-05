50ff86aca3e4625fb1168fe2b650f0274e5ff2fd
Oliver Partida <oliver.partida@sage.com>
2022-08-05 23:08:54 +0200

    feat: useEffect

---
+++ react-hooks/src/components/StarRating.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import Star from "./Star";
+
+const StarRating =  ({ totalStars = 5 }) => {
+  const [selectedStars, setSelectedStars] = useState(0);
+  const createArray = (length) => [...Array(length)];
+
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+      <p>
+        {selectedStars} of {totalStars}{" "}
+      </p>
+    </>
+  );
+};
+
+export default StarRating;
--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -1,34 +1,33 @@
-import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { FaStar } from "react-icons/fa";
+// import StarRating from "./components/StarRating";
+import React, { useState, useEffect } from "react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-function Star({ selected = false, onSelect }) {
-  return <FaStar color={selected ? "red" : "gray"} onClick={onSelect} />;
-}
+function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(`https://api.github.com/users`);
+      const data = await result.json();
+      setData(data);
+    };
+    fetchData();
+  },[]);
 
-function StarRating({ totalStars = 5 }) {
-  const [selectedStars, setSelectedStars] = useState(0);
-  return (
-    <>
-      {createArray(totalStars).map((n, i) => (
-        <Star
-          key={i}
-          selected={selectedStars > i}
-          onSelect={() => setSelectedStars(i + 1)}
-        />
-      ))}
-      <p>{selectedStars} of {totalStars} </p>
-    </>
-  );
-}
-
-const createArray = (length) => [...Array(length)];
-
-function App({ name }) {
-  return <StarRating />;
+  return data.length
+    ? data && (
+        <>
+          <ul>
+            {data.map((user) => (
+              <li key={user.id}>{user.login}</li>
+            ))}
+          </ul>
+          <button onClick={()=> setData([])}>Remove Data</button>
+        </>
+      )
+    : null;
 }
 
 root.render(
