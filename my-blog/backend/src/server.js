84483ff14e29b89481ae9598c28266e7573af28a
Oliver Partida <oliver.partida@sage.com>
2022-08-05 13:23:52 +0200

    fix: add upvote endpoint

--- my-blog/backend/src/server.js
+++ my-blog/backend/src/server.js
@@ -1,19 +1,37 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-
 const app = express();
 
 app.use(bodyParser.json());
 
-app.post("/api/articles/:name/upvote", (req, response) => {
-  const articleName = req.params.name;
+app.post("/api/articles/:name/upvote", async (req, response) => {
+  try {
+    const { MongoClient } = require("mongodb");
+    const articleName = req.params.name;
+    const uri = "mongodb://localhost:27017";
+    const client = new MongoClient(uri);
 
-  response
-    .status(200)
-    .send(
-      `${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!!`
-    );
+    await client.connect();
+    const db = client.db("my-blog");
+
+    const articleInfo = await db.collection('articles').findOne({ name: articleName });
+    console.log(articleInfo)
+    await db
+      .collection("articles")
+      .updateOne(
+        { name: articleName },
+        { '$set': { upvotes: articleInfo.upvotes + 1 } }
+      );
+    const updateArticleInfo = await db.collection('articles').findOne({
+      name: articleName,
+    });
+    response.status(200).json(updateArticleInfo);
+  } catch (error) {
+    response.status(500).json({ message: "Error connecting to db", error });
+  } finally {
+    await client.close();
+  }
 });
 
 app.post("/api/articles/:name/add-comment", (req, response) => {
@@ -29,9 +47,9 @@ app.get("/api/articles/:name", async (req, response) => {
     const articleName = req.params.name;
     const uri = "mongodb://localhost:27017";
     const client = new MongoClient(uri);
-    
+
     await client.connect();
-    
+
     const db = client.db("my-blog");
 
     const articleInfo = await db
@@ -39,8 +57,6 @@ app.get("/api/articles/:name", async (req, response) => {
       .findOne({ name: articleName });
 
     response.status(200).json(articleInfo);
-    
-
   } catch (error) {
     response.status(500).json({ message: "Error connecting to db", error });
   } finally {
