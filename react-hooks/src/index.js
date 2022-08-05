70b4897ba54aaf1c65cf3d86d64147ffe6b344c0
Oliver Partida <oliver.partida@sage.com>
2022-08-05 23:44:43 +0200

    feat: add useEffect to component

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -3,46 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 // import StarRating from "./components/StarRating";
 // import Users from "./components/Users";
-import { useReducer } from "react";
+// import Message from "./components/Message";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function App() {
-  const [number, setNumber] = useReducer(
-    (number, newNumber) => number + newNumber,
-    0
-  );
-  const initialState = {
-    message: "hi",
-  };
-
-  function reducer(state, action) {
-    switch (action.type) {
-      case "yell":
-        return {
-          message: `HEY I JUST SAID ${state.message}`,
-        };
-      case "whisper":
-        return {
-          message: `Excuse me, I just said ${state.message}`,
-        };
-        default:
-          return {message: "Unkwown"}
-    }
-  }
-  const [checked, toggle] = useReducer((checked) => !checked, false);
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  return (
-    <>
-      <h1 onClick={() => setNumber(1)}>{number}</h1>
-      <input type="checkbox" value={checked} onChange={toggle} />
-      {checked ? "checked" : "not checked"}
-      <p>Message: {state.message}</p>
-      <button onClick={()=>dispatch({type: "yell"})}>YELL</button>
-      <button onClick={()=>dispatch({type: "whisper"})}>WHISPER</button>
-    </>
-  );
+  
 }
 
 root.render(
